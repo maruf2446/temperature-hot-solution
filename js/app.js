abb6da5681de10f9bf75bd2ecd7006b26b163d66
maruf2446 <maruf2446@gmail.com>
2023-03-04 11:26:59 -0800

    first commit

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
-const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
-const searchTemperature = () => {
-    const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+// // DO NOT SHOW API KEY ON YOUR JS FILE
+const API_KEY = `d2b6ba37e101f56cc0b2fa514f1df51d`
 
+const loadTemperature = city =>{
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     fetch(url)
-        .then(res => res.json())
-        .then(data => displayTemperature(data));
+    .then(res => res.json())
+    .then(data => displayTemperature(data));
 }
 
-const setInnerText = (id, text) => {
-    document.getElementById(id).innerText = text;
+const displayTemperature = data =>{
+    // const temperature = document.getElementById('temperature');
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+    console.log(data.weather[0].main);
 }
 
-const displayTemperature = temperature => {
-    setInnerText('city', temperature.name);
-    setInnerText('temperature', temperature.main.temp);
-    setInnerText('condition', temperature.weather[0].main);
-    // set weather icon
-    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
-    const imgIcon = document.getElementById('weather-icon');
-    imgIcon.setAttribute('src', url);
+const setInnerTextById = (id, text) =>{
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
 }
+
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city
+    document.getElementById('city').innerText = city;
+    
+    loadTemperature(city);
+})
+
+
+loadTemperature('dhaka');
